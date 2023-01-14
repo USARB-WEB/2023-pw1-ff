79cc9d632b8ee98a80208eb377a785caad64fd46
sergiuchilat <chilatsergiu@gmail.com>
2023-01-14 09:56:15 +0200

    JavaScript syntax

---
+++ 2 - JavaScript syntax/app.js
@@ -0,0 +1,12 @@
+console.log('Hello. I am a browser console')
+
+let x = 2;
+let y = 3;
+let sum = x + y;
+console.log(`Sum = ${sum}`)
+
+let r = 1;
+const pi = 3.14;
+let circleSquare = pi * r**2;
+console.log(`Circle square = ${circleSquare}`);
+
