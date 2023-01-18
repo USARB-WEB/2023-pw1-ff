5c3983478868186f463a70c66b3719e5da04879d
sergiuchilat <chilatsergiu@gmail.com>
2023-01-18 13:57:48 +0200

    JS DOM

---
+++ 6 - DOM/app.js
@@ -0,0 +1,7 @@
+document.getElementById('div').innerHTML = 'I am JS script';
+document.getElementById('name').value = 'Ion Creanga';
+document.querySelectorAll('#list li').forEach(li => {
+    li.style.backgroundColor = 'blue';
+})
+
+document.body.style.backgroundColor = 'yellow';
